84403f4f35a12a36a083f16653f8492c52661331
mmkqy <meky@email.com>
2023-11-10 13:57:13 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { useState,useEffect } from "react";
+import Header from "./components/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+function App() {
+  const [showForm, setShowForm] = useState(false)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Task 1",
+    },
+    {
+      id: 2,
+      text: "Task 2",
+    },
+    {
+      id: 3,
+      text: "Task 3",
+    },
+  ]);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    console.log(newTodos)
+    setTodos(newTodos)
+  }
+
+  const addTodo = (text) => {
+    const id = todos.length + 1;
+    const newTodo = { id, text };
+   setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div className="App">
+      <Header title="Todo App" addTodo = {() => setShowForm(!showForm)} showForm={showForm}/>
+      {showForm ? <AddTodo onAdd={addTodo}/>:''}
+     {todos.length > 0 ?  <Todos todos={todos} onDelete={deleteTodo} />:<p>Task is empty.</p>}
+    </div>
+  );
+}
+
+export default App;
